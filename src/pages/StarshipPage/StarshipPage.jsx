18295eb3867bb95798a4964d9a9e569f65626402
HiepTrinh <xmikecolx@gmail.com>
2022-10-12 17:05:47 -0500

    add return link

--- src/pages/StarshipPage/StarshipPage.jsx
+++ src/pages/StarshipPage/StarshipPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import { getDetails } from '../../services/sw-api'
+import { Link } from 'react-router-dom'
 
 const StarShipPage = () => {
   const [shipDetails, setShipDetails] = useState({})
@@ -19,6 +20,7 @@ const StarShipPage = () => {
           <>
             <h2>{shipDetails.name}</h2>
             <h2>{shipDetails.model}</h2>
+            <Link to='/'>Home</Link>
           </>
           :
           <>
